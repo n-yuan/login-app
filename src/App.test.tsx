d2920b93ea15f16ac0bcc1d9555b2ad071dca9e4
Nan Yuan <nan.yuan.dev@gmail.com>
2021-01-09 23:46:07 +1100

    Add enzyme and config test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import App from "./App";
+import { shallow, mount } from "enzyme";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Testing", () => {
+  let wrapper: any;
+  beforeEach(() => {
+    wrapper = mount(<App />);
+  });
+  test("Render the ttile of Login App", () => {
+    expect(wrapper.find("h1").text()).toContain("Login App");
+  });
 });
