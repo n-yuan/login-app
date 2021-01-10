cb00ecde274ebfb2335d58a62387d9ebf66b4536
Nan Yuan <nan.yuan.dev@gmail.com>
2021-01-10 13:26:09 +1100

    Add API path

---
+++ src/constants/path.ts
@@ -0,0 +1,8 @@
+// Using existing API for login
+// API come from previous project team-logger-api
+// Repo: https://github.com/n-yuan/team-logger-api
+export const API_URL = "https://team-logger-api.herokuapp.com";
+
+export const API_PATH = {
+  login: () => `/api/auth`,
+};
